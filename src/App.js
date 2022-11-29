fb45aa28b4adcfd104f0225a75a51804423f963f
adtuul <adtuul@ttu.ee>
2022-11-29 14:12:35 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./style.css";
 import Die from "./components/Die";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 
-export default function App() {
-  const [dice, setDice] = useState(allNewDice());
+const generateNewDie = () => {
+  return {
+    value: Math.ceil(Math.random() * 6),
+    isHeld: false,
+    id: nanoid(),
+  };
+};
 
+const allNewDice = () => {
+  const newDice = [];
+  for (let i = 0; i < 10; i++) {
+    newDice.push(generateNewDie());
+  }
+  return newDice;
+};
+
+const App = () => {
+  const [dice, setDice] = useState(allNewDice());
   const [tenzies, setTenzies] = useState(false);
 
   useEffect(() => {
@@ -18,23 +33,7 @@ export default function App() {
     }
   }, [dice]);
 
-  function generateNewDie() {
-    return {
-      value: Math.ceil(Math.random() * 6),
-      isHeld: false,
-      id: nanoid(),
-    };
-  }
-
-  function allNewDice() {
-    const newDice = [];
-    for (let i = 0; i < 10; i++) {
-      newDice.push(generateNewDie());
-    }
-    return newDice;
-  }
-
-  function rollDice() {
+  const rollDice = () => {
     if (!tenzies) {
       setDice((oldDice) =>
         oldDice.map((die) => {
@@ -45,22 +44,23 @@ export default function App() {
       setTenzies(false);
       setDice(allNewDice());
     }
-  }
+  };
 
-  function holdDice(id) {
+  const holdDice = (id) => {
     setDice((oldDice) =>
       oldDice.map((die) => {
         return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
       })
     );
-  }
+  };
 
   const diceElements = dice.map((die) => (
     <Die
       value={die.value}
       key={die.id}
+      id={die.id}
       isHeld={die.isHeld}
-      holdDice={() => holdDice(die.id)}
+      holdDice={holdDice}
     />
   ));
 
@@ -78,4 +78,6 @@ export default function App() {
       </button>
     </main>
   );
-}
+};
+
+export default App;
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,11 +1,19 @@
-export default function Die(props) {
+import React from "react";
+
+const Die = ({ value, id, isHeld, holdDice }) => {
   const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "white",
+    backgroundColor: isHeld ? "#59E391" : "white",
+  };
+
+  const handleClick = () => {
+    holdDice(id);
   };
 
   return (
-    <div className="die-face" style={styles} onClick={props.holdDice}>
-      <h2 className="die-num">{props.value}</h2>
+    <div className="die-face" style={styles} onClick={handleClick}>
+      <h2 className="die-num">{value}</h2>
     </div>
   );
-}
+};
+
+export default Die;
